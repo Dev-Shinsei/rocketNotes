0df7cb27e95e84c5d0163813aadef19ca3ec70ea
Carlos Gomes <dev.shinsei@gmail.com>
2022-07-04 20:18:36 -0300

    Finishing usersController, update server.js and creating and connecting SQLite Database

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,13 @@
+const AppError = require("../utils/AppError");
+
 class UsersController {
   create(request, response) {
     const { name, email, password } = request.body;
+
+    if (!name) {
+      throw new AppError("Name is Required!");
+    }
+
     response.json({ name, email, password });
   }
 }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,16 +4,8 @@ const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router();
 
-function myMiddleware(request, response, next) {
-  console.log("You've passed trough the middleware");
-  if (!request.body.isAdmin) {
-  }
-
-  next();
-}
-
 const usersController = new UsersController();
 
-usersRoutes.post("/", myMiddleware, usersController.create);
+usersRoutes.post("/",  usersController.create);
 
 module.exports = usersRoutes;
